d355d5fab776d0179faba012355e68a86c872a8a
Hector Sanjuan <code@hector.link>
2019-08-12 23:51:25 +0200

    Add example

---
+++ examples/globaldb/globaldb.go
@@ -0,0 +1,281 @@
+package main
+
+// This is a CLI that lets you join a global permissionless CRDT-based
+// database using CRDTs and IPFS.
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+
+	ds "github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore/query"
+	crdt "github.com/ipfs/go-ds-crdt"
+	logging "github.com/ipfs/go-log"
+
+	"github.com/libp2p/go-libp2p"
+	connmgr "github.com/libp2p/go-libp2p-connmgr"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+
+	ipfslite "github.com/hsanjuan/ipfs-lite"
+	"github.com/mitchellh/go-homedir"
+
+	multiaddr "github.com/multiformats/go-multiaddr"
+)
+
+var (
+	logger    = logging.Logger("globaldb")
+	listen, _ = multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/33123")
+	topic     = "globaldb-example"
+	config    = "globaldb-example"
+)
+
+func main() {
+	logging.SetLogLevel("*", "warn")
+	ctx, cancel := context.WithCancel(context.Background())
+
+	dir, err := homedir.Dir()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	data := filepath.Join(dir, "globaldb-example")
+
+	store, err := ipfslite.BadgerDatastore(data)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer store.Close()
+
+	keyPath := filepath.Join(data, "key")
+	var priv crypto.PrivKey
+	_, err = os.Stat(keyPath)
+	if os.IsNotExist(err) {
+		priv, _, err = crypto.GenerateKeyPair(crypto.Ed25519, 1)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		data, err := crypto.MarshalPrivateKey(priv)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		err = ioutil.WriteFile(keyPath, data, 0400)
+		if err != nil {
+			logger.Fatal(err)
+		}
+	} else if err != nil {
+		logger.Fatal(err)
+	} else {
+		key, err := ioutil.ReadFile(keyPath)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		priv, err = crypto.UnmarshalPrivateKey(key)
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+	}
+	pid, err := peer.IDFromPublicKey(priv.GetPublic())
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	connman := connmgr.NewConnManager(100, 400, time.Minute)
+
+	h, dht, err := ipfslite.SetupLibp2p(
+		ctx,
+		priv,
+		nil,
+		[]multiaddr.Multiaddr{listen},
+		libp2p.ConnectionManager(connman),
+	)
+	defer h.Close()
+	defer dht.Close()
+
+	psub, err := pubsub.NewGossipSub(ctx, h)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	ipfs, err := ipfslite.New(ctx, store, h, dht, nil)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	pubsubBC, err := crdt.NewPubSubBroadcaster(ctx, psub, "globaldb-example")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	opts := crdt.DefaultOptions()
+	opts.Logger = logger
+	opts.RebroadcastInterval = 10 * time.Second
+	opts.PutHook = func(k ds.Key, v []byte) {
+		fmt.Printf("Added: [%s] -> %s\n", k, string(v))
+
+	}
+	opts.DeleteHook = func(k ds.Key) {
+		fmt.Printf("Removed: [%s]\n", k)
+	}
+
+	crdt, err := crdt.New(store, ds.NewKey("crdt"), ipfs, pubsubBC, opts)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer crdt.Close()
+
+	fmt.Println("Bootstrapping...")
+
+	bstr, _ := multiaddr.NewMultiaddr("/ip4/94.130.135.167/tcp/33123/ipfs/12D3KooWFta2AE7oiK1ioqjVAKajUJauZWfeM7R413K7ARtHRDAu")
+	inf, _ := peer.AddrInfoFromP2pAddr(bstr)
+	list := append(ipfslite.DefaultBootstrapPeers(), *inf)
+	ipfs.Bootstrap(list)
+	connman.TagPeer(inf.ID, "keep", 100)
+
+	fmt.Printf(`
+Peer ID: %s
+Listen address: %s
+Topic: %s
+Data Folder: %s
+
+Ready!
+
+Commands:
+
+> list               -> list items in the store
+> get <key>          -> get value for a key
+> put <key> <value>  -> store value on a key
+> exit               -> quit
+
+
+`,
+		pid, listen, topic, data,
+	)
+
+	if len(os.Args) > 1 && os.Args[1] == "daemon" {
+		fmt.Println("Running in daemon mode")
+		go func() {
+			for {
+				fmt.Printf("%s - %d connected peers\n", time.Now().Format(time.Stamp), len(connectedPeers(h)))
+				time.Sleep(10 * time.Second)
+			}
+		}()
+		signalChan := make(chan os.Signal, 20)
+		signal.Notify(
+			signalChan,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGHUP,
+		)
+		<-signalChan
+		cancel()
+		return
+	}
+
+	fmt.Printf("> ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		fields := strings.Fields(text)
+		if len(fields) == 0 {
+			fmt.Printf("> ")
+			continue
+		}
+
+		cmd := fields[0]
+
+		switch cmd {
+		case "exit", "quit":
+			cancel()
+			return
+		case "debug":
+			if len(fields) < 2 {
+				fmt.Println("debug <on/off/peers>")
+			}
+			st := fields[1]
+			switch st {
+			case "on":
+				logging.SetLogLevel("globaldb", "debug")
+			case "off":
+				logging.SetLogLevel("globaldb", "error")
+			case "peers":
+				for _, p := range connectedPeers(h) {
+					addrs, err := peer.AddrInfoToP2pAddrs(p)
+					if err != nil {
+						logger.Warning(err)
+						continue
+					}
+					for _, a := range addrs {
+						fmt.Println(a)
+					}
+				}
+			}
+		case "list":
+			q := query.Query{}
+			results, err := crdt.Query(q)
+			if err != nil {
+				printErr(err)
+			}
+			for r := range results.Next() {
+				if r.Error != nil {
+					printErr(err)
+					continue
+				}
+				fmt.Printf("[%s] -> %s\n", r.Key, string(r.Value))
+			}
+		case "get":
+			if len(fields) < 2 {
+				fmt.Println("get <key>")
+				fmt.Println("> ")
+				continue
+			}
+			k := ds.NewKey(fields[1])
+			v, err := crdt.Get(k)
+			if err != nil {
+				printErr(err)
+				continue
+			}
+			fmt.Printf("[%s] -> %s\n", k, string(v))
+		case "put":
+			if len(fields) < 3 {
+				fmt.Println("put <key> <value>")
+				fmt.Println("> ")
+				continue
+			}
+			k := ds.NewKey(fields[1])
+			v := strings.Join(fields[2:], " ")
+			err := crdt.Put(k, []byte(v))
+			if err != nil {
+				printErr(err)
+				continue
+			}
+		}
+		fmt.Printf("> ")
+	}
+}
+
+func printErr(err error) {
+	fmt.Println("error:", err)
+	fmt.Println("> ")
+}
+
+func connectedPeers(h host.Host) []*peer.AddrInfo {
+	var pinfos []*peer.AddrInfo
+	for _, c := range h.Network().Conns() {
+		pinfos = append(pinfos, &peer.AddrInfo{
+			ID:    c.RemotePeer(),
+			Addrs: []multiaddr.Multiaddr{c.RemoteMultiaddr()},
+		})
+	}
+	return pinfos
+}
---
+++ examples/globaldb/go.mod
@@ -0,0 +1,42 @@
+module github.com/ipfs/go-ds-crdt/examples/globaldb
+
+go 1.12
+
+require (
+	github.com/btcsuite/btcd v0.0.0-20190807005414-4063feeff79a // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/davidlazar/go-crypto v0.0.0-20190522120613-62389b5e4ae0 // indirect
+	github.com/dgraph-io/badger v1.6.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20190812055157-5d271430af9f // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/hsanjuan/ipfs-lite v0.1.5
+	github.com/ipfs/go-bitswap v0.1.6 // indirect
+	github.com/ipfs/go-blockservice v0.1.2 // indirect
+	github.com/ipfs/go-cid v0.0.3 // indirect
+	github.com/ipfs/go-datastore v0.0.5
+	github.com/ipfs/go-ds-crdt v0.1.0
+	github.com/ipfs/go-ipfs-config v0.0.7 // indirect
+	github.com/ipfs/go-ipfs-provider v0.2.1 // indirect
+	github.com/ipfs/go-ipld-cbor v0.0.3 // indirect
+	github.com/ipfs/go-log v0.0.1
+	github.com/ipfs/go-unixfs v0.2.1 // indirect
+	github.com/libp2p/go-eventbus v0.1.0 // indirect
+	github.com/libp2p/go-libp2p v0.3.0
+	github.com/libp2p/go-libp2p-connmgr v0.1.1
+	github.com/libp2p/go-libp2p-core v0.2.2
+	github.com/libp2p/go-libp2p-kad-dht v0.2.0 // indirect
+	github.com/libp2p/go-libp2p-pubsub v0.1.0
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/multiformats/go-multiaddr v0.0.4
+	github.com/multiformats/go-multiaddr-dns v0.0.3 // indirect
+	github.com/polydawn/refmt v0.0.0-20190807091052-3d65705ee9f1 // indirect
+	github.com/smartystreets/assertions v1.0.1 // indirect
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
+	go.opencensus.io v0.22.0 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20190812172437-4e8604ab3aff // indirect
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
+)
